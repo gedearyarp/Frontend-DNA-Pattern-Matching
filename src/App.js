467345d7dda991cef92af37ba41e2e477033cb85
I Gede Arya <arya.raditya@mekari.com>
2022-04-18 18:19:50 +0700

    add react route dan meniapkan template pages n component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import UploadDNA from './pages/UploadDNA';
+import CheckDNA from './pages/CheckDNA';
+import SearchDNA from './pages/SearchDNA';
+
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  theme
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/check-dna" element={<CheckDNA/>}/>
+        <Route path="/upload-dna" element={<UploadDNA/>}/>
+        <Route path="/search-dna" element={<SearchDNA/>}/>
+      </Routes>
     </ChakraProvider>
   );
 }
