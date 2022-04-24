9baba034ad2155b7478ce376bc2bfd27f560e1bb
GedeAryaRP <71829426+gedearyarp@users.noreply.github.com>
2022-04-24 16:42:49 +0700

    balikin home ke semula karena jadi hancur.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,64 +21,61 @@ import {
 function Home() {
     return (
         <Flex w="100%" bg="#FFFFFE" alignItems="center" justifyContent="center">
-            <Stack h="100%" d="flex" flexDir="column" alignItems="center" justifyContent="center">
-                <Box h="80%" pt="12%">
+            <Stack d="flex" flexDir="column" h="90vh" alignItems="center" justifyContent="center">
+                {/* LOGO WEBSITE */}
+                <Box>
+                    <Image src={dnaLogo} height="20vh" mx="auto" alt='DNA Prasmanan Logo'/>
+                </Box>
 
-                    {/* LOGO WEBSITE */}
-                    <Box pb="1.5%" h="10%">
-                        <Image src={dnaLogo} h="auto" maxH="153" mx="auto" alt='DNA Prasmanan Logo'/>
-                    </Box>
+                {/* LANDING TEXT */}
+                <Box>
+                    <Text fontSize="5xl" align="center" fontWeight="bold" color="#2B2C34" mb={6}>
+                        Identify Your Disease <br/>
+                        With DNAmatch
+                    </Text>
+                </Box>
 
-                    {/* LANDING TEXT */}
-                    <Box>
-                        <Text fontSize="42px" align="center" fontWeight="bold" color="#2B2C34" mb="3.5%">
-                            Identify Your Disease <br/>
-                            With DNAmatch
-                        </Text>
-                    </Box>
+                {/* BUTTONS */}
+                <Box>
+                    <Center>
+                        <Stack spacing="15px">
+                            <Link to="/check-dna">
+                                <Button px="25px" py="20px" bg="#E45858" border='4px' borderColor="#2B2C34" borderRadius='30px' w="100%">
+                                    <Text color="#FFFFFE" fontSize="21px" m="3">
+                                        Test Your DNA
+                                    </Text>
+                                    <Spacer />
+                                    <ArrowForwardIcon color="#FFFFFE" w={6} h={6}/>
+                                </Button>
+                            </Link>
 
-                    {/* BUTTONS */}
-                    <Box>
-                        <Center>
-                            <Stack spacing="15px">
-                                <Link to="/check-dna">
-                                    <Button px="25px" py="20px" bg="#E45858" border='4px' borderColor="#2B2C34" borderRadius='30px' w="100%">
-                                        <Text color="#FFFFFE" fontSize="21px" m="3">
-                                            Test Your DNA
-                                        </Text>
-                                        <Spacer />
-                                        <ArrowForwardIcon color="#FFFFFE" w={6} h={6}/>
-                                    </Button>
-                                </Link>
+                            <Link to="/search-dna">
+                                <Button px="25px" py="20px" bg="#6246EA" border='4px' borderColor="#2B2C34" borderRadius='30px' w="100%">
+                                    <Text color="#FFFFFE" fontSize="21px" m={3}>
+                                        Search History
+                                    </Text>
+                                    <Spacer />
+                                    <ArrowForwardIcon color="#FFFFFE" w={6} h={6}/>
+                                </Button>
+                            </Link>
 
-                                <Link to="/search-dna">
-                                    <Button px="25px" py="20px" bg="#6246EA" border='4px' borderColor="#2B2C34" borderRadius='30px' w="100%">
-                                        <Text color="#FFFFFE" fontSize="21px" m={3}>
-                                            Search History
-                                        </Text>
-                                        <Spacer />
-                                        <ArrowForwardIcon color="#FFFFFE" w={6} h={6}/>
-                                    </Button>
-                                </Link>
-
-                                <Link to="/upload-dna">
-                                    <Button px="25px" py="20px" bg="#6246EA" border='4px' borderColor="#2B2C34" borderRadius='30px' w="100%">
-                                        <Text color="#FFFFFE" fontSize="21px" m={3}>
-                                            Upload DNA Disease
-                                        </Text>
-                                        <Spacer />
-                                        <ArrowForwardIcon color="#FFFFFE" w={6} h={6}/>
-                                    </Button>
-                                </Link>
-                            </Stack>
-                        </Center>
-                    </Box>
+                            <Link to="/upload-dna">
+                                <Button px="25px" py="20px" bg="#6246EA" border='4px' borderColor="#2B2C34" borderRadius='30px' w="100%">
+                                    <Text color="#FFFFFE" fontSize="21px" m={3}>
+                                        Upload DNA Disease
+                                    </Text>
+                                    <Spacer />
+                                    <ArrowForwardIcon color="#FFFFFE" w={6} h={6}/>
+                                </Button>
+                            </Link>
+                        </Stack>
+                    </Center>
                 </Box>
 
                 {/* Watermark */}
-                <Box h="20%" pt="10%">
-                    <Center>
-                        <Box  pb="1%">
+                <Box>
+                    <Center >
+                        <Box position="fixed" bottom="20px">
                             <Text align="center">
                                 © 2022 prasmanan.
                             </Text>
@@ -89,6 +86,8 @@ function Home() {
                     </Center>
                 </Box>
             </Stack>
+            
+            
         </Flex>
     )
 }
